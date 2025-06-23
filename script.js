a7ab3609500b87a8a77c1c14bfed389b7d5c692d
gabrielbastos02 <kamasuabiel@gmail.com>
2025-06-23 20:44:38 -0300

    padaria
    pao

---
+++ script.js
@@ -0,0 +1,11 @@
+//hamburgerButton
+//Aqui adicinando um elemento de escuatar o click dou mouse sobre o icone de hamburguer.
+mobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.add("flex");
+    //criando uma funça que a ouvir o click dou mouse, ele adiciona uma class (flex), que esta fazando nossa caixa (div) visivel, ou seja, ficara totalmente visivel para todos.
+
+});
+
+closeButton.addEventListener("click", function () {
+    mobileMenu.classList.remove("flex");
+});
