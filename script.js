a638594acdaa22ec86c7562ae471139a37853e9a
gabrielbastos02 <kamasuabiel@gmail.com>
2025-06-25 20:26:43 -0300

    padaria

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+window.revelar = ScrollReveal({reset:true});
+revelar.reveal('.efeito-img-topo', {
+    //estamos chanando a class a qual eu quero o efeito do scrollRevel.
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.clientes-efeito', {
+    duration: 2000,
+    distance: '120px',
+    origin: 'left'
+});
+
+
+revelar.reveal('.principais-produtos', {
+    duration: 2000,
+    distance: '120px'
+});
+
+revelar.reveal('.sobre-nos', {
+    duration: 2000,
+    distance: '120px',
+    origin: 'right'
+});
+
 //hamburgerButton
 //Aqui adicinando um elemento de escuatar o click dou mouse sobre o icone de hamburguer.
 hamburgerButton.addEventListener("click", function () {
@@ -9,3 +34,4 @@ hamburgerButton.addEventListener("click", function () {
 closeButton.addEventListener("click", function () {
     mobileMenu.classList.remove("flex");
 });
+
